230103ff712e194e8cc8356ace7990251e7c27f7
TaridaG <abdulkadirtaridag@gmail.com>
2025-12-05 23:41:46 +0300

    maven build kuruldu ve entegre edildi. gerekli bağımlılıklar kurulup entegre edildi. dosya yolları güncellendi.

---
+++ src/main/java/com/devopsanddevsecops/testing/DevsecopsTestingApplication.java
@@ -0,0 +1,12 @@
+package com.devopsanddevsecops.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsecopsTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevsecopsTestingApplication.class, args);
+    }
+}
